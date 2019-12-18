307d3668a388cc0142ef0ed9de8c7b8767f19848
Bruno Garcia <brwnow@gmail.com>
2019-12-18 18:58:31 -0300

    Avoiding copy constructor and assignment operator to be implicitly defined for singleton class MainWindow

--- source/view/include/mainwindow.hpp
+++ source/view/include/mainwindow.hpp
@@ -10,6 +10,9 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    MainWindow(MainWindow const &) = delete;
+    void operator = (MainWindow const &) = delete;
+
     ~MainWindow();
 
     static MainWindow& getInstance();
