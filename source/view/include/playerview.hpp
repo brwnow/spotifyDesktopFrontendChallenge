5e900ecef9e86badff838ed6da7e1cb5c15cdbbf
Bruno Garcia <brwnow@gmail.com>
2019-12-19 03:22:58 -0300

    Creating PlayerView basic structure. Implementing initialization methods

--- source/view/include/playerview.hpp
+++ source/view/include/playerview.hpp
@@ -1,6 +1,9 @@
 #ifndef PLAYER_VIEW_HPP
 #define PLAYER_VIEW_HPP
 
+#include <QLabel>
+#include <QPushButton>
+#include <QSlider>
 #include <QWidget>
 
 class PlayerView : public QWidget
@@ -10,6 +13,34 @@ class PlayerView : public QWidget
 public:
     PlayerView(QWidget *parent = nullptr);
     ~PlayerView();
+
+    bool getIsPlaying() const;
+
+signals:
+    void playClicked();
+    void pauseClicked();
+    void nextClicked();
+    void previousClicked();
+    void songPositionChanged(int);
+
+public slots:
+    void setPlaying(bool playing);
+    void setupSong(const QString &songName, int resolution);
+    void updateSongProgress(int songPosition, const QString &currentTime);
+    void setNoSong();
+
+private:
+    QLabel *songName, *currentTime;
+    QPushButton *playPauseButton, *previousSongButton, *nextSongButton;
+    QSlider *navigationBar;
+    bool isPlaying;
+
+    void setupWidgets();
+    void setupLayout();
+    void setupConnections();
+
+private slots:
+    void onPlayPauseButtonClicked();
 
 };
 
