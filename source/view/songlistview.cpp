a2844c2d2003698539974a878c256f6732d1c36c
Bruno Garcia <brwnow@gmail.com>
2019-12-19 17:33:21 -0300

    Creating basic SongListView structure. Create signals, slots and initialization methods. Adding title and container for songs

--- source/view/songlistview.cpp
+++ source/view/songlistview.cpp
@@ -1,11 +1,66 @@
 #include "songlistview.hpp"
 
-SongListView::SongListView(QWidget *parent)
-{
+#include <exception>
 
+#include <QVBoxLayout>
+
+using std::exception;
+
+SongListView::SongListView(QWidget *parent) :
+    QWidget(parent)
+{
+    setupWidgets();
+    setupLayout();
+    setupConnections();
 }
 
 SongListView::~SongListView()
 {
 
+}
+
+void SongListView::createSong(const QString &songName, int songID)
+{
+    Q_UNUSED(songName);
+    Q_UNUSED(songID);
+}
+
+void SongListView::removeSong(int songID)
+{
+    Q_UNUSED(songID);
+}
+
+void SongListView::setupWidgets()
+{
+    playlistName = new QLabel();
+    listOfSongs = new QListWidget();
+}
+
+void SongListView::setupLayout()
+{
+    QVBoxLayout *layout = new QVBoxLayout();
+
+    layout->addWidget(playlistName, 0);
+    layout->addWidget(listOfSongs, 1);
+
+    this->setLayout(layout);
+}
+
+void SongListView::setupConnections()
+{
+    connect(listOfSongs, SIGNAL(itemActivated(QListWidgetItem*)), this, SLOT(onSongSelected(QListWidgetItem*)));
+}
+
+void SongListView::onSongSelected(QListWidgetItem *songItem)
+{
+    Q_UNUSED(songItem);
+
+    try
+    {
+
+    }
+    catch(exception &ex)
+    {
+
+    }
 }
