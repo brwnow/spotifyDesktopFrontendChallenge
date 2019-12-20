464fd0ff1acc70612a425aa8ae34b8af3d3adc68
Bruno Garcia <brwnow@gmail.com>
2019-12-20 17:17:00 -0300

    Implemented basic SongListController signals, public slots and attributes

--- source/controller/include/songlistcontroller.hpp
+++ source/controller/include/songlistcontroller.hpp
@@ -7,6 +7,26 @@ class SongListController : public QObject
 {
 public:
     SongListController(QObject *parent = nullptr);
+
+signals:
+    void songCreated(const QString &title, int songID);
+    void songRemoved(int songID);
+    void playlistCleared();
+
+public slots:
+    void createSong(const QString &title, int songID);
+    void removeSong(int songID);
+    void loadPlaylist(int playlistId);
+
+    // Clear the loaded playlist. If playlistId is INVALID_PLAYLIST_ID then
+    // clear the playlist no matter what id it has, otherwise clear the playlist
+    // only if the given playlistId matches the loaded playlist id
+    void clearPlaylist(int playlistId = INVALID_PLAYLIST_ID);
+
+private:
+    static const int INVALID_PLAYLIST_ID;
+
+    int openedPlaylistId;
 };
 
 #endif // SONG_LIST_CONTROLLER_HPP
