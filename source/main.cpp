e66ab65289488bad33da2903a9312373da3f9dd5
Bruno Garcia <brwnow@gmail.com>
2019-12-20 00:02:40 -0300

    Changing main function to call Core static method that start the application. QApplication must be defined in main function because Core class is a Q_OBJECT

--- source/main.cpp
+++ source/main.cpp
@@ -1,11 +1,12 @@
 #include "mainwindow.hpp"
 
+#include <core.hpp>
+
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow &w = MainWindow::getInstance();
-    w.show();
-    return a.exec();
+    QApplication app(argc, argv);
+
+    return Core::run(app);
 }
