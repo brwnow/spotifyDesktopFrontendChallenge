d8601f867299e0399e1de571bf1b3852b9ef973e
Bruno Garcia <brwnow@gmail.com>
2019-12-20 18:48:02 -0300

    binding SongListController to SongTable. Also improving some methods signature.

--- source/controller/songlistcontroller.cpp
+++ source/controller/songlistcontroller.cpp
@@ -2,29 +2,51 @@
 
 const int SongListController::INVALID_PLAYLIST_ID = -1;
 
-SongListController::SongListController(QObject *parent) :
+SongListController::SongListController(QSqlDatabase &database, QObject *parent) :
     QObject(parent),
+    songTable(database),
     openedPlaylistId(INVALID_PLAYLIST_ID)
 {
+    connect(&songTable, SIGNAL(songInserted(const QString&, int)),
+            this, SIGNAL(songCreated(const QString&, int)));
+    connect(&songTable, SIGNAL(songRemoved(int)),
+            this, SLOT(songRemoved(int)));
 }
 
-void SongListController::createSong(const QString &title, int songID)
+void SongListController::createSong(const QString &title)
 {
-    Q_UNUSED(title);
-    Q_UNUSED(songID);
+    songTable.insert(title);
 }
 
 void SongListController::removeSong(int songID)
 {
-    Q_UNUSED(songID);
+    songTable.remove(songID);
 }
 
 void SongListController::loadPlaylist(int playlistId)
 {
-    Q_UNUSED(playlistId);
+    if(playlistId != INVALID_PLAYLIST_ID)
+    {
+        list<SongTable::Tuple> playlist = songTable.getPlaylist(playlistId);
+
+        if(openedPlaylistId != INVALID_PLAYLIST_ID)
+            clearPlaylist();
+
+        openedPlaylistId = playlistId;
+
+        for(list<SongTable::Tuple>::iterator it = playlist.begin(); it != playlist.end(); ++it)
+        {
+            emit songCreated(it->name, it->id);
+        }
+    }
 }
 
 void SongListController::clearPlaylist(int playlistId)
 {
-    Q_UNUSED(playlistId);
+    if(playlistId == INVALID_PLAYLIST_ID || playlistId == openedPlaylistId)
+    {
+        openedPlaylistId = INVALID_PLAYLIST_ID;
+
+        emit playlistCleared();
+    }
 }
