5e47d429b6c5fe9f50ae653a00055b94bd40b5f5
Bruno Garcia <brwnow@gmail.com>
2019-12-20 18:12:14 -0300

    Implementation of SongTable model class.

--- source/model/database/include/songtable.hpp
+++ source/model/database/include/songtable.hpp
@@ -1,13 +1,40 @@
 #ifndef SONG_TABLE_HPP
 #define SONG_TABLE_HPP
 
+#include <list>
+
 #include <QObject>
 #include <QSqlDatabase>
 
+using std::list;
+
 class SongTable : public QObject
 {
+    Q_OBJECT
+
 public:
+    static const QString ID_FIELD;
+    static const QString NAME_FIELD;
+
+    class Tuple
+    {
+    public:
+        const int id;
+        const QString name;
+
+        Tuple(int id, const QString &name);
+    };
+
     SongTable(QSqlDatabase &database, QObject *parent = nullptr);
+
+    list<Tuple> getPlaylist(int playlistID);
+    void insert(const QString &title);
+    void remove(int id);
+
+signals:
+    void songInserted(const QString &title, int id);
+    void insertionFailed(const QString &title);
+    void songRemoved(int id);
 
 private:
     QSqlDatabase &database;
--- source/model/database/songtable.cpp
+++ source/model/database/songtable.cpp
@@ -1,7 +1,62 @@
 #include "songtable.hpp"
 
+#include <QSqlQuery>
+#include <QSqlRecord>
+#include <QVariant>
+
+const QString SongTable::ID_FIELD("ID");
+const QString SongTable::NAME_FIELD("NAME");
+
 SongTable::SongTable(QSqlDatabase &database, QObject *parent) :
     QObject(parent),
     database(database)
+{
+}
+
+list<SongTable::Tuple> SongTable::getPlaylist(int playlistID)
+{
+    QSqlQuery query(database);
+    query.prepare("SELECT ID, NAME FROM PLAYLIST WHERE PLAYLIST_ID = ?");
+    query.bindValue(0, playlistID);
+    int idFieldNum = query.record().indexOf(ID_FIELD);
+    int nameFieldNum = query.record().indexOf(NAME_FIELD);
+    list<Tuple> playlist;
+
+    while(query.next())
+    {
+        int id = query.value(idFieldNum).toInt();
+        QString name = query.value(nameFieldNum).toString();
+
+        playlist.push_back(Tuple(id, name));
+    }
+
+    return playlist;
+}
+
+void SongTable::insert(const QString &title)
+{
+    QSqlQuery query(database);
+    query.prepare("INSERT INTO SONG (NAME) VALUES (?)");
+    query.bindValue(0, title);
+
+    if(query.exec())
+        emit songInserted(title, query.lastInsertId().toInt());
+    else
+        emit insertionFailed(title);
+}
+
+void SongTable::remove(int id)
+{
+    QSqlQuery query(database);
+    query.prepare("DELETE FROM SONG WHERE ID = ?");
+    query.bindValue(0, id);
+
+    if(query.exec())
+        emit songRemoved(id);
+}
+
+SongTable::Tuple::Tuple(int id, const QString &name) :
+    id(id),
+    name(name)
 {
 }
