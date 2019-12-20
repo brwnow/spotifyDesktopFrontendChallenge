865337311343e3e6db247acfe5bb835b60bc4fe1
Bruno Garcia <brwnow@gmail.com>
2019-12-20 03:37:37 -0300

    Implemented class PlaylistTable which represents and interacts with table PLAYLIST in database

---
+++ source/model/database/include/playlisttable.hpp
@@ -0,0 +1,44 @@
+#ifndef PLAYLIST_TABLE_HPP
+#define PLAYLIST_TABLE_HPP
+
+#include <list>
+
+#include <QObject>
+#include <QSqlDatabase>
+
+using std::list;
+
+class PlaylistTable : QObject
+{
+    Q_OBJECT
+
+public:
+    static const QString ID_FIELD;
+    static const QString NAME_FIELD;
+
+    class Tuple
+    {
+    public:
+        const int id;
+        const QString name;
+
+        Tuple(int id, const QString name);
+    };
+
+    PlaylistTable(QSqlDatabase &database);
+
+    list<Tuple> getAllPlaylists();
+    void insert(const QString &title);
+    void remove(int id);
+
+signals:
+    void playlistInserted(int id);
+    void insertionFailed(const QString &title);
+    void playlistRemoved(int id);
+
+private:
+    QSqlDatabase &database;
+
+};
+
+#endif // PLAYLIST_TABLE_HPP
